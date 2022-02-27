98ed346d63f9d51b8140ac032544bdd7d03048ee
Hocine Slimani <78388940+Askeladd-sensei@users.noreply.github.com>
2022-02-27 23:47:14 +0100

    Create hocine.php

---
+++ hocine.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Acces Valid </title>
+</head>
+
+<body style="background-color:#2f2f2f">
+  <br>
+  <br>
+<h1 style="font-size:80px;text-align:center;color:white"> Votre accès est valide <h1>
+  <h1 style="font-size:240px; text-align:center;"> &#x2705 <h1>
+<h2 style="font-size:80px;"> En cours de construction a 3emmi <h2>
+</body>
